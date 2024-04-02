387fa87a237f6fc4ca3421b0bdafee87d00b5f1a
Nhuengzii <32nhueng@gmail.com>
2024-04-02 14:40:44 +0700

    add helm

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import os
 
 app = FastAPI()
 
-@app.get("/")
+API_PREFIX=os.environ.get("API_PREFIX")
+
+@app.get(API_PREFIX)
 def read_root():
     return {"Hello": "World", "env": os.environ.get('HELLO')}
 
